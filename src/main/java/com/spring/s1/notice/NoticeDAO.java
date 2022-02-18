302645a6773f470bef2d23eb82414af8f04d2486
SujinKim-sj <sjsjkim0@gmail.com>
2022-02-18 09:28:59 +0900

    Step 2 - Add Notice

---
+++ src/main/java/com/spring/s1/notice/NoticeDAO.java
@@ -0,0 +1,35 @@
+package com.spring.s1.notice;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class NoticeDAO {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	private final String NAMESPACE = "com.spring.s1.notice.NoticeDAO.";
+	
+	//Detail
+	public NoticeDTO detail(NoticeDTO noticeDTO) throws Exception{
+		return sqlSession.selectOne(NAMESPACE + "detail", noticeDTO);
+	}
+	
+	//List
+	public List<NoticeDTO> list() throws Exception{
+		return sqlSession.selectList(NAMESPACE + "list");
+	}
+	
+	//Insert
+	public int add(NoticeDTO noticeDTO) throws Exception{
+		return sqlSession.insert(NAMESPACE + "add", noticeDTO);
+	}
+	
+	//Delete
+	public int delete(NoticeDTO noticeDTO) throws Exception{
+		return sqlSession.delete(NAMESPACE + "delete", noticeDTO);
+	}
+}
