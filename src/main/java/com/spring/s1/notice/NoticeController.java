2ff3998e4894dfe2347a9a9ec3e1d9f15d49ffb9
SujinKim-sj <sjsjkim0@gmail.com>
2022-02-21 09:44:40 +0900

    Add Notice

---
+++ src/main/java/com/spring/s1/notice/NoticeController.java
@@ -0,0 +1,49 @@
+package com.spring.s1.notice;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/notice/*")
+public class NoticeController {
+
+	@Autowired
+	private NoticeService noticeService;
+	
+	//add
+	@RequestMapping(value = "add", method = RequestMethod.POST)
+	public String add(NoticeDTO noticeDTO) throws Exception {
+		int result = noticeService.add(noticeDTO);
+		
+		return "redirect:./list";
+	}
+	
+	@RequestMapping(value = "add", method = RequestMethod.GET)
+	public void add() throws Exception {
+
+	}
+	
+	//detail
+	@RequestMapping(value = "detail", method = RequestMethod.GET) 
+	public void detail(Model model, NoticeDTO noticeDTO) throws Exception {
+		noticeDTO = noticeService.detail(noticeDTO);
+		model.addAttribute("detail", noticeDTO);
+	}
+	
+	//list
+	@RequestMapping(value = "list", method = RequestMethod.GET)
+	public ModelAndView list(ModelAndView mv) throws Exception{
+		
+		List<NoticeDTO> ar = noticeService.list();
+		mv.addObject("list", ar);
+		mv.setViewName("notice/list");
+		
+		return mv;
+	}
+}
