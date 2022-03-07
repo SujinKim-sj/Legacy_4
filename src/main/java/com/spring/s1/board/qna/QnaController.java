0e4c94e34b7170f1ed129567b2375cb30b08ef4c
SujinKim-sj <sjsjkim0@gmail.com>
2022-03-07 17:54:10 +0900

    Add QnA

---
+++ src/main/java/com/spring/s1/board/qna/QnaController.java
@@ -0,0 +1,89 @@
+package com.spring.s1.board.qna;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.s1.board.BoardDTO;
+import com.spring.s1.util.Pager;
+
+@Controller
+@RequestMapping(value = "/qna/*")
+public class QnaController {
+	
+	@Autowired
+	private QnaService qnaService;
+	
+	@ModelAttribute("board")
+	public String board() {
+		return "qna";
+	}
+	
+	//add
+	@RequestMapping(value = "add", method = RequestMethod.POST)
+	public ModelAndView add(QnaDTO qnaDTO) throws Exception {
+		ModelAndView mv = new ModelAndView();
+		int result = qnaService.add(qnaDTO);
+		mv.setViewName("redirect:./list");
+		return mv;
+	}
+	
+	@RequestMapping(value = "add", method = RequestMethod.GET)
+	public ModelAndView add() throws Exception {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("board/add");
+		return mv;
+	}
+	
+	//update
+	@RequestMapping(value = "update", method = RequestMethod.POST)
+	public ModelAndView update(QnaDTO qnaDTO) throws Exception {
+		ModelAndView mv = new ModelAndView();
+		int result = qnaService.update(qnaDTO);
+		mv.setViewName("redirect:./list");
+		return mv;
+	}
+	
+	@RequestMapping(value = "update", method = RequestMethod.GET)
+	public ModelAndView update(Model model, QnaDTO qnaDTO) throws Exception {
+		ModelAndView mv = new ModelAndView();
+		BoardDTO boardDTO = qnaService.detail(qnaDTO);
+		mv.addObject("update", boardDTO);
+		mv.setViewName("board/update");
+		return mv;
+	}
+	
+	//delete
+	@RequestMapping(value = "delete", method = RequestMethod.GET)
+	public String delete(QnaDTO qnaDTO) throws Exception {
+		int result = qnaService.delete(qnaDTO);
+		return "redirect:./list";
+	}
+	
+	//detail
+	@RequestMapping(value = "detail", method = RequestMethod.GET)
+	public String detail(QnaDTO qnaDTO, Model model) throws Exception {
+		BoardDTO boardDTO = qnaService.detail(qnaDTO);
+		model.addAttribute("detail", boardDTO);
+		
+		return "board/detail";
+	}
+	
+	//list
+	@RequestMapping(value = "list", method = RequestMethod.GET)
+	public ModelAndView list(ModelAndView mv, Pager pager) throws Exception {
+		List<BoardDTO> ar = qnaService.list(pager);
+		
+		mv.addObject("list", ar);
+		mv.addObject("pager", pager);
+		mv.setViewName("board/list");
+		
+		return mv;
+	}
+}
